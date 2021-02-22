d84cb23a54190dbe9d067f4aa0ba37ca2344aa20
FabrizioPe <fab.pellegr@gmail.com>
2021-02-22 13:08:44 +0100

    Chap. 16 draft solution (all tests passed but one)

---
+++ 16_scrambler/scrambler.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Author : FabrizioPe
+Date   : 2021-02-22
+Purpose: Scramble the middle letters of words
+"""
+
+import argparse
+import os
+import random
+import re
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Scramble the letters of words',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('str',
+                        metavar='str',
+                        help='Input text or file')
+
+    parser.add_argument('-s',
+                        '--seed',
+                        help='Random seed',
+                        metavar='int',
+                        type=int,
+                        default=None)
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.str):
+        args.str = open(args.str).read()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    random.seed(args.seed)
+    text = args.str
+    lines = text.splitlines()  # list of lines (recall that \n is lost!)
+
+    for line in lines:
+        words = re.split(r'(\W+)', line)
+        # print(words)
+        print(''.join(map(scramble, words)))
+
+
+# --------------------------------------------------
+def scramble(word):
+    """Return word with randomised middle part"""
+    if len(word) <= 3:
+        return word
+
+    middle_part = list(word[1:-1])
+    random.shuffle(middle_part)  # scramble it!
+    return word[0] + ''.join(middle_part) + word[-1]
+
+
+# --------------------------------------------------
+def test_scramble():
+    state = random.getstate()
+    random.seed(1)
+    assert scramble('the') == 'the'
+    assert scramble('an') == 'an'
+    assert scramble('hello') == 'hlleo'
+    assert scramble('mountain') == 'maiutonn'
+    random.setstate(state)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
